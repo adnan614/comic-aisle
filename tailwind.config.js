b747f6e0ff10e35982dfa6a38eb88974b168a338
adnan614 <adnanfaruque23@gmail.com>
2021-12-17 09:27:40 +0600

    design

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      width: {
+        450: "100rem",
+      },
+      colors: {
+        mygray: {
+          50: "#777e8c",
+        },
+        myyellow:{
+          50: "#fff500",
+        }
+      },
+    },
+  },
+  plugins: [],
+};
